1f0e3f7dbf80cf2929fd6610d0bf2f9e2a35bd77
stephan.laemmer <stephan.laemmer@735d13b6-9817-0410-8766-e36946ffe9aa>
2008-08-08 14:45:38 +0000

    Extension basis layout created with Extension kickstarter
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/twitter_frontend/trunk@11038 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ ext_emconf.php
@@ -0,0 +1,44 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext: "twitter_frontend"
+#
+# Auto generated 08-08-2008 16:43
+#
+# Manual updates:
+# Only the data in the array - anything else is removed by next write.
+# "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'twitter',
+	'description' => 'Shows last twitter status of a defined user. More features are planned.',
+	'category' => 'fe',
+	'author' => 'Stephan Lämmer',
+	'author_email' => 'kaffdaddy@insomniaonline.de',
+	'shy' => '',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'module' => '',
+	'state' => 'alpha',
+	'internal' => '',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearCacheOnLoad' => 0,
+	'lockType' => '',
+	'author_company' => '',
+	'version' => '0.0.0',
+	'constraints' => array(
+		'depends' => array(
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => 'a:17:{s:9:"ChangeLog";s:4:"8e1a";s:10:"README.txt";s:4:"ee2d";s:12:"ext_icon.gif";s:4:"1bdc";s:17:"ext_localconf.php";s:4:"cde6";s:14:"ext_tables.php";s:4:"d401";s:13:"locallang.xml";s:4:"dc33";s:16:"locallang_db.xml";s:4:"863a";s:19:"doc/wizard_form.dat";s:4:"6639";s:20:"doc/wizard_form.html";s:4:"8254";s:14:"pi1/ce_wiz.gif";s:4:"02b6";s:36:"pi1/class.tx_twitterfrontend_pi1.php";s:4:"072a";s:44:"pi1/class.tx_twitterfrontend_pi1_wizicon.php";s:4:"371d";s:13:"pi1/clear.gif";s:4:"cc11";s:17:"pi1/locallang.xml";s:4:"6262";s:24:"pi1/static/editorcfg.txt";s:4:"da54";s:28:"static/Twitter/constants.txt";s:4:"ce8a";s:24:"static/Twitter/setup.txt";s:4:"dabd";}',
+);
+
+?>
